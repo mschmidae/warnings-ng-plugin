fd3d8fb2388c09c0bba5b2f712146f30f48ded66
Michael Schmid <michael.anian.schmid@gmail.com>
2019-05-25 11:03:45 +0200

    Distributed execution - copy file to agent

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DistributedExecutionITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DistributedExecutionITest.java
@@ -1,15 +1,22 @@
 package io.jenkins.plugins.analysis.warnings.recorder;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import org.jenkinsci.test.acceptance.docker.DockerClassRule;
 import org.jenkinsci.test.acceptance.docker.fixtures.JavaContainer;
 import hudson.model.FreeStyleProject;
+import hudson.model.Queue;
 import hudson.model.Slave;
 import hudson.plugins.sshslaves.SSHLauncher;
+import hudson.security.SecurityRealm;
 import hudson.slaves.DumbSlave;
+import hudson.tasks.Shell;
 
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
 
 public class DistributedExecutionITest extends IntegrationTestWithJenkinsPerSuite {
@@ -21,10 +28,22 @@ public class DistributedExecutionITest extends IntegrationTestWithJenkinsPerSuit
     @Test
     public void distributionWithDumpSlave() throws Exception {
         DumbSlave slave = JENKINS_PER_SUITE.createSlave();
-        FreeStyleProject project = createFreeStyleProjectWithWorkspaceFiles("eclipse_4_Warnings.txt");
+        /*
+        FreeStyleProject project = createFreeStyleProject();
 
+        getJenkins().jenkins.getQueue().schedule(project).getFuture().get();
+
+        copySingleFileToAgentWorkspace(slave, project, "eclipse_4_Warnings.txt", "eclipse_4_Warnings-issues.txt");
         enableEclipseWarnings(project);
-        getJenkins().jenkins.getQueue().schedule(project);
+
+
+        getJenkins().jenkins.getQueue().schedule(project).getFuture().get();
+
+        List<AnalysisResult> result = getAnalysisResults(project.getLastBuild());
+        System.out.println(result.size());
+
+*/
+        executeOnWorker(slave);
 
         System.out.println("End");
 
@@ -37,12 +56,32 @@ public class DistributedExecutionITest extends IntegrationTestWithJenkinsPerSuit
         getJenkins().jenkins.addNode(slave);
         getJenkins().waitOnline(slave);
 
-        FreeStyleProject project = createFreeStyleProjectWithWorkspaceFiles("eclipse_4_Warnings.txt");
+        //FreeStyleProject project = createFreeStyleProjectWithWorkspaceFiles("eclipse_4_Warnings.txt");
 
-        enableEclipseWarnings(project);
-        getJenkins().jenkins.getQueue().schedule(project);
+        //enableEclipseWarnings(project);
+        //getJenkins().jenkins.getQueue().schedule(project);
+
+        executeOnWorker(slave);
 
         System.out.println("End");
 
+    }
+
+    private void executeOnWorker(final Slave worker) throws ExecutionException, InterruptedException {
+        FreeStyleProject project = createFreeStyleProject();
+
+        System.out.println(getJenkins().jenkins.getSecurity());
+        System.out.println(getJenkins().jenkins.getSecurityRealm());
+        //ToDo Security
+        getJenkins().jenkins.getQueue().schedule(project).getFuture().get();
+
+        copySingleFileToAgentWorkspace(worker, project, "eclipse_4_Warnings.txt", "eclipse_4_Warnings-issues.txt");
+        enableEclipseWarnings(project);
+
+
+        getJenkins().jenkins.getQueue().schedule(project).getFuture().get();
+
+        List<AnalysisResult> result = getAnalysisResults(project.getLastBuild());
+        System.out.println(result.size());
     }
 }
