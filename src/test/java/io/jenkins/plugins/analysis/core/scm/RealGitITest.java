d5c7106887fcc1e454c4de009b5850985f3a1d8f
Michael Schmid <michael.anian.schmid@gmail.com>
2019-05-26 00:57:51 +0200

    Zwischenstand

--- src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
@@ -2,7 +2,10 @@ package io.jenkins.plugins.analysis.core.scm;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.CreateFileBuilder;
 
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.model.Run;
@@ -51,9 +54,13 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
 
     @Test
     public void gitBlameDifferentDirectory() throws Exception {
+        //CentralDifferenceSolver.cpp
+        //LCPcalc.cpp
+
         sampleRepo.init();
-        sampleRepo.write("sub/init.txt", "init");
-        sampleRepo.git("add", "sub/init.txt");
+        sampleRepo.write("CentralDifferenceSolver.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
+        sampleRepo.write("LCPcalc.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
+        sampleRepo.git("add", "CentralDifferenceSolver.cpp", "LCPcalc.cpp");
         sampleRepo.git("commit", "--all", "--message=init");
 
         GitSCMBuilder builder = new GitSCMBuilder(new SCMHead("master"), null, sampleRepo.fileUrl(), null);
@@ -62,14 +69,99 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
         GitSCM git = builder.build();
 
         FreeStyleProject project = createFreeStyleProject();
+        /*
+        createFileInWorkspace(project, "build/doxygen/doxygen/doxygen.log",
+                "Notice: Output directory `build/doxygen/doxygen' does not exist. I have created it for you.\n"
+                + "src/CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\n"
+                + "The command that should end the block seems to be missing!\n"
+                + " \n"
+                + "src/LCPcalc.cpp:12: Warning: the name `lcp_lexicolemke.c' supplied as the second argument in the \\file statement is not an input file");
+        */
         copySingleFileToWorkspace(project, "doxygen.log", "build/doxygen/doxygen/doxygen.log");
         project.setScm(git);
+        project.getBuildersList().add(new CreateFileBuilder("build/doxygen/doxygen/doxygen.log",
+                "Notice: Output directory `build/doxygen/doxygen' does not exist. I have created it for you.\n"
+                        + "src/CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\n"
+                        + "The command that should end the block seems to be missing!\n"
+                        + " \n"
+                        + "src/LCPcalc.cpp:12: Warning: the name `lcp_lexicolemke.c' supplied as the second argument in the \\file statement is not an input file"));
+
+
+        IssuesRecorder recorder = enableWarnings(project, createTool(new Doxygen(), "build/doxygen/doxygen/doxygen.log"));
+        recorder.setBlameDisabled(true);
+
+        recorder.setAggregatingResults(true);
+        recorder.setEnabledForFailure(true);
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        result.getBlames().getRequests().forEach(r -> r.getCommit(1));
+
+        System.out.println("End");
+    }
+
+    @Test
+    public void gitBlameDifferentDirectoryPipeline() throws Exception {
+        //CentralDifferenceSolver.cpp
+        //LCPcalc.cpp
+
+        sampleRepo.init();
+        sampleRepo.write("CentralDifferenceSolver.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
+        sampleRepo.write("LCPcalc.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
+        sampleRepo.git("add", "CentralDifferenceSolver.cpp", "LCPcalc.cpp");
+        sampleRepo.git("commit", "--all", "--message=init");
+
+        GitSCMBuilder builder = new GitSCMBuilder(new SCMHead("master"), null, sampleRepo.fileUrl(), null);
+        RelativeTargetDirectory extension = new RelativeTargetDirectory("src"); // JENKINS-57260
+        builder.withExtension(extension);
+        GitSCM git = builder.build();
+
+        WorkflowJob project = createPipeline();
+
+        project.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "agent any\n"
+                + "options{\n"
+                + "skipDefaultCheckout()\n"
+                + "}\n"
+                + "stages{\n"
+                + "stage('Prepare') {\n"
+                + "  steps {\n"
+                + "    dir('src') {\n"
+                + "      checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: '" + sampleRepo.fileUrl() + "']]])\n"
+                + "    }\n"
+                + "  }\n"
+                + "}\n"
+                + "stage('Doxygen') {\n"
+                + "  steps {\n"
+                + "    dir('build/doxygen') {\n"
+                + "      writeFile file: 'doxygen/doxygen.log', text: '\"Notice: Output directory doc/doxygen/framework does not exist. I have created it for you.\\nsrc/CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\\nThe command that should end the block seems to be missing!\\nsrc/LCPcalc.cpp:12: Warning: the name lcp_lexicolemke.c supplied as the second argument in the file statement is not an input file\"'\n"
+                + "    }\n"
+                + "    recordIssues(aggregatingResults: true, enabledForFailure: true, tools: [ doxygen(name: 'Doxygen', pattern: 'build/doxygen/doxygen/doxygen.log') ] )\n"
+                + "  }\n"
+                + "}\n"
+                + "}}", false));
+        /*
+        createFileInWorkspace(project, "build/doxygen/doxygen/doxygen.log",
+                "Notice: Output directory `build/doxygen/doxygen' does not exist. I have created it for you.\n"
+                + "src/CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\n"
+                + "The command that should end the block seems to be missing!\n"
+                + " \n"
+                + "src/LCPcalc.cpp:12: Warning: the name `lcp_lexicolemke.c' supplied as the second argument in the \\file statement is not an input file");
+        */
+        /*
+        copySingleFileToWorkspace(project, "doxygen.log", "build/doxygen/doxygen/doxygen.log");
+        project.setScm(git);
+        project.getBuildersList().add(new CreateFileBuilder("build/doxygen/doxygen/doxygen.log",
+                "Notice: Output directory `build/doxygen/doxygen' does not exist. I have created it for you.\n"
+                        + "CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\n"
+                        + "The command that should end the block seems to be missing!\n"
+                        + " \n"
+                        + "LCPcalc.cpp:12: Warning: the name `lcp_lexicolemke.c' supplied as the second argument in the \\file statement is not an input file"));
 
 
         IssuesRecorder recorder = enableWarnings(project, createTool(new Doxygen(), "build/doxygen/doxygen/doxygen.log"));
         recorder.setAggregatingResults(true);
         recorder.setEnabledForFailure(true);
-
+*/
         AnalysisResult result = scheduleSuccessfulBuild(project);
         result.getBlames().getRequests().forEach(r -> r.getCommit(1));
 
